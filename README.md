47bd0018ed01093cae92d1c77efb2769f8c4a879
zhouning188 <zhouning188@gmail.com>
2015-10-24 03:12:22 -1000

    edit Code.txt and edit README.md and edit run_analysis.R

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Getting-and-Cleaning-Data-Project
 Prepare tidy data 
+
+# Script design
+
+Step 1. Merges the training and the test sets into one dataset
+* Get data from the list of features’ name in ”features.txt”
+* Attributes label to “X_train”,“subject_train”,“ y_train”
+* Combine“X_train”,“subject_train”,“ y_train”to"train_set”
+* Attributes label to ”X_test”,“subject_test”,“ y_test”
+* Combine”X_test”,“subject_test”,“ y_test”to"test_set”
+* Combine “train_set”and”test_set”to”data_set”
+
+Step 2. Extract the measurements on the mean and deviation 
+* Find target strings that include ”-mean” and “-std”
+* Subset target dataset 
+* Combine the “subject” and “activity” in one subset 
+
+Step 3. Name the activities in the data set 
+Master$activities <- as.character(Master$activities)
+Master$activities[Master$activities == 1] <- "Walking"
+Master$activities[Master$activities == 2] <- "Walking Upstairs"
+Master$activities[Master$activities == 3] <- "Walking Downstairs"
+Master$activities[Master$activities == 4] <- "Sitting"
+Master$activities[Master$activities == 5] <- "Standing"
+Master$activities[Master$activities == 6] <- "Laying"
+Master$activities <- as.factor(Master$activities)
+
+Step 4. Label the descriptive variable names
+* Extract the old names of variables from the merged data set
+* Use regular expressions and “sub()” function to update the names to more  descriptive ones
+* Replace the variables’ names with the updated vector of names in the merged data set
+
+Step 5. Creates a tidy data set 
+* Fix the rows to show mean values 
+* rename the data
+write the latest data set using write.talbe()
+
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -40,36 +40,6 @@ names(Master) <- gsub("Gyro", "Gyroscope", names(Master))
 names(Master) <- gsub("^t", "time", names(Master))
 names(Master) <- gsub("^f", "frequency", names(Master))
 Master$participants <- as.character(Master$participants)
-Master$participants[Master$participants == 1] <- "Participant 1"
-Master$participants[Master$participants == 2] <- "Participant 2"
-Master$participants[Master$participants == 3] <- "Participant 3"
-Master$participants[Master$participants == 4] <- "Participant 4"
-Master$participants[Master$participants == 5] <- "Participant 5"
-Master$participants[Master$participants == 6] <- "Participant 6"
-Master$participants[Master$participants == 7] <- "Participant 7"
-Master$participants[Master$participants == 8] <- "Participant 8"
-Master$participants[Master$participants == 9] <- "Participant 9"
-Master$participants[Master$participants == 10] <- "Participant 10"
-Master$participants[Master$participants == 11] <- "Participant 11"
-Master$participants[Master$participants == 12] <- "Participant 12"
-Master$participants[Master$participants == 13] <- "Participant 13"
-Master$participants[Master$participants == 14] <- "Participant 14"
-Master$participants[Master$participants == 15] <- "Participant 15"
-Master$participants[Master$participants == 16] <- "Participant 16"
-Master$participants[Master$participants == 17] <- "Participant 17"
-Master$participants[Master$participants == 18] <- "Participant 18"
-Master$participants[Master$participants == 19] <- "Participant 19"
-Master$participants[Master$participants == 20] <- "Participant 20"
-Master$participants[Master$participants == 21] <- "Participant 21"
-Master$participants[Master$participants == 22] <- "Participant 22"
-Master$participants[Master$participants == 23] <- "Participant 23"
-Master$participants[Master$participants == 24] <- "Participant 24"
-Master$participants[Master$participants == 25] <- "Participant 25"
-Master$participants[Master$participants == 26] <- "Participant 26"
-Master$participants[Master$participants == 27] <- "Participant 27"
-Master$participants[Master$participants == 28] <- "Participant 28"
-Master$participants[Master$participants == 29] <- "Participant 29"
-Master$participants[Master$participants == 30] <- "Participant 30"
 Master$participants <- as.factor(Master$participants)
 Master.dt <- data.table(Master)
 TidyData <- Master.dt[, lapply(.SD, mean), by = 'participants,activities']
